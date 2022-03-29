b46c94396307a1ee89acd05bdda1611a9cabefde
jalieven <jan.lievens@gmail.com>
2022-03-29 16:26:46 +0200

    add variable metadata to put uploads

--- parallel-upload-download/parallel-put.go
+++ parallel-upload-download/parallel-put.go
@@ -35,14 +35,27 @@ import (
 // Change this value to test with a different object size.
 const defaultObjectSize = 10 * 1024 * 1024
 
+const defaultMetaCount = 1
+const defaultMetaSize = 1024
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func RandStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
 // Uploads all the inputs objects in parallel, upon any error this function panics.
-func parallelUploads(objectNames []string, data []byte) {
+func parallelUploads(objectNames []string, data []byte, metaCount int, metaSize int) {
 	var wg sync.WaitGroup
 	for _, objectName := range objectNames {
 		wg.Add(1)
 		go func(objectName string) {
 			defer wg.Done()
-			if err := uploadBlob(data, objectName); err != nil {
+			if err := uploadBlob(data, objectName, metaCount, metaSize); err != nil {
 				panic(err)
 			}
 		}(objectName)
@@ -51,7 +64,7 @@ func parallelUploads(objectNames []string, data []byte) {
 }
 
 // uploadBlob does an upload to the S3/Minio server
-func uploadBlob(data []byte, objectName string) error {
+func uploadBlob(data []byte, objectName string, metaCount int, metaSize int) error {
 	credsUp := credentials.NewStaticCredentials(os.Getenv("ACCESSKEY"), os.Getenv("SECRETKEY"), "")
 	sessUp := session.New(aws.NewConfig().
 		WithCredentials(credsUp).
@@ -62,11 +75,19 @@ func uploadBlob(data []byte, objectName string) error {
 	uploader := s3manager.NewUploader(sessUp, func(u *s3manager.Uploader) {
 		u.PartSize = 64 * 1024 * 1024 // 64MB per part
 	})
+
+	meta := make(map[string]string)
+	for n := 0; n <= metaCount.Value; n++ {
+		meta[fmt.Sprintf("%s-%v", "test-metadata-key", metaCount)] = aws.String(RandStringBytes(1024))
+	}
+
 	var err error
 	_, err = uploader.Upload(&s3manager.UploadInput{
 		Body:   bytes.NewReader(data),
 		Bucket: aws.String(os.Getenv("BUCKET")),
 		Key:    aws.String(objectName),
+		MetadataDirective: aws.String("REPLACE"),
+		Metadata: meta,
 	})
 
 	return err
@@ -74,6 +95,8 @@ func uploadBlob(data []byte, objectName string) error {
 
 var (
 	objectSize = flag.Int("size", defaultObjectSize, "Size of the object to upload.")
+	metaCount = flag.Int("meta-count", defaultMetaCount, "Metadata entry count of the object to upload.")
+	metaSize = flag.Int("meta-size", defaultMetaSize, "Metadata size of each entry of the object to upload.")
 )
 
 func main() {
@@ -93,7 +116,7 @@ func main() {
 	var data = bytes.Repeat([]byte("a"), *objectSize)
 
 	start := time.Now().UTC()
-	parallelUploads(objectNames, data)
+	parallelUploads(objectNames, data, metaCount, metaSize)
 
 	totalSize := conc * *objectSize
 	elapsed := time.Since(start)
